3ee3b4d9f1a3003e09976f297eb6f78f0ebf2674
Marcus Karl <mlk140030@utdallas.edu>
2016-04-21 16:54:04 -0500

    changes

--- Project3/src/Client/ClientMain.java
+++ Project3/src/Client/ClientMain.java
@@ -21,8 +21,22 @@ public class ClientMain
         //Exit menu loop when user chooses option 8
         boolean exit = false;
         
+        //Scanner for menu choices
+        Scanner menuScan = new Scanner(System.in);
+        //Scanner for options within menu choices
+        Scanner msgScan = new Scanner(System.in);
+        
         //Inform server of user name and connection
         MsgCommObj msg = new MsgCommObj();
+        
+        while (!Character.isAlphabetic(uN.charAt(0)))
+        {
+        	System.out.print("Your username must start with an alphebetic character. \n"
+        			+ "Enter in a new user name: ");
+        	
+        	uN = msgScan.nextLine();
+        }
+        
         msg.setFromUserName(uN);
         msg.setUserOption(0);
         
@@ -31,13 +45,13 @@ public class ClientMain
             //Write the user name to the server via the message object
             sStream.writeObject(msg);
             
-            System.out.println("Sent name to server.");
-            
             msg = (MsgCommObj)cStream.readObject();
-            System.out.println(msg.getUserMsg() + "\n");
+            System.out.println(msg.getUserMsg());
             
             if(msg.getUserOption() == -1)
             {
+            	msgScan.close();
+            	menuScan.close();
                 return;
             }
             
@@ -46,11 +60,6 @@ public class ClientMain
         {
             System.out.println("ERROR: " + ex);
         }
-        
-        //Scanner for menu choices
-        Scanner menuScan = new Scanner(System.in);
-        //Scanner for options within menu choices
-        Scanner msgScan = new Scanner(System.in);
         
         //Main menu loop that runs until the client wants to exit and terminate connection
         do 
@@ -110,9 +119,17 @@ public class ClientMain
                         //Set choice in message
                         msg.setUserOption(menuChoice);
                         //Prompt user for message recipients user name
-                        System.out.print("Enter message recipients name: ");
+                        String toName = null;
+                        
+                        do
+                        {
+	                        System.out.print("Enter message recipients name: ");
+	                        toName = msgScan.nextLine();
+                        }
+                        while ( !Character.isAlphabetic((Character)toName.charAt(0)) );
+                        
                         //Set To: field in message
-                        msg.setToUserName(msgScan.nextLine());
+                        msg.setToUserName(toName);
                         System.out.println();
                         //Prompt user for message to send
                         System.out.print("Enter message for " + msg.getToUserName() + ": ");
@@ -121,16 +138,23 @@ public class ClientMain
                         System.out.println();
                         //Send message to server for specified recipient
                         sStream.writeObject(msg);
+                        
+                        // Reads response from server
+                        msg = (MsgCommObj)cStream.readObject();
+                        
+                        // Prints user response
+                        System.out.println(msg.getUserMsg());
+                        
                         break;
                         
                     case 4: //Send a message to all connected users
                         //Set choice in message
                         msg.setUserOption(menuChoice);
                         //Prompt user for message to send
-                        System.out.println("Enter message for all connected users: ");
-                        System.out.println();
+                        System.out.print("Enter message for all connected users: ");
                          //Set message data to user input
                         msg.setUserMsg(msgScan.nextLine());
+                        System.out.println();
                         //Send message to server for all connected users 
                         sStream.writeObject(msg);
                         break;
@@ -140,9 +164,9 @@ public class ClientMain
                         msg.setUserOption(menuChoice);
                         //Prompt user for message to send
                         System.out.println("Enter message for all known users: ");
-                        System.out.println();
                         //Set message data to user input
                         msg.setUserMsg(msgScan.nextLine());
+                        System.out.println();
                         //Send message to server for all known users
                         sStream.writeObject(msg);
                         break;
@@ -154,20 +178,20 @@ public class ClientMain
                         //Write menu choice to server
                         sStream.writeObject(msg);
                         
-                        //Loop to get messages from server as long as option in message recieved from server is zero 
-                        do
+                        // Receives first response from server
+                        msg = (MsgCommObj)cStream.readObject();
+                        
+                        //Loop to get messages from server as long as option in message received from server is zero 
+                        while(msg.getUserOption() != -1)
                         {
-                            //Read message from server
-                            msg = (MsgCommObj)cStream.readObject();
-                            
-                            if (msg.getUserOption() == 0) {
                             //Add message data to linked list
                             msgList.addLast(msg);
                             //increment count for total unread messages
-                            msgCount++;
-                            }
+                            msgCount++;                            
+                            
+                            // Gets next message from server
+                            msg = (MsgCommObj)cStream.readObject();
                         }
-                        while(msg.getUserOption()== 0);
                         
                         //Prompt user amount of unread messages they have
                         System.out.println("You have " + msgCount + " unread messages");
@@ -177,46 +201,54 @@ public class ClientMain
                     case 7: //read user messages
                         int msgNum = 0;
                         
-                        //Loop to read messages while input is valid message menu number
-                        do
+                        if(msgCount == 0)
                         {
-                            //Print all unread message from linked list with from user name and time stamp
-                            if(msgCount == 0)
-                            {
-                                System.out.println("You have no unread messages");
-                            }
-                            else
-                            {
-                                System.out.println("Unread Messages:");
-                            }
-                            
-                            for(int i = 0; i < msgCount; i++)
-                            {
-                                //Copy message at ith position from linked list
-                                msg = msgList.get(i);
-                                //Print reference menu number, From: field, and time stamp
-                                System.out.println((i+1) + ". From: " + msg.getFromUserName() + " at " + msg.getDateTime());
-                            }
-                            
-                            //Prompt user to input a menu number to read corresponding message
-                            System.out.println();
-                            System.out.print("Enter number to display message or any other key to return to main menu: ");
-                            //Get user input
-                            msgNum = msgScan.nextInt();
-                            System.out.println();
-                            
-                            //If valid menu input
-                            if(msgNum > 0 && msgNum <= msgCount)
-                            {
-                                //Print the message requested and delete from linked list
-                                msg = msgList.remove(msgNum - 1);
-                                System.out.println("Message From " + msg.getFromUserName() + ": " + msg.getUserMsg());
-                                System.out.println();
-                                //Decrement unread messages
-                                msgCount--;
-                            }
+                            System.out.println("You have no unread messages.\n");
                         }
-                        while(msgNum > 0 && msgNum <= msgCount);
+                        else
+                        {
+                            System.out.println("Unread Messages:");
+                            
+                          //Loop to read messages while input is valid message menu number
+                            do
+                            {
+                                //Print all unread message from linked list with from user name and time stamp
+                                
+                                
+                                for(int i = 0; i < msgCount; i++)
+                                {
+                                    //Copy message at i'th position from linked list
+                                	msg = msgList.get(i);
+                                    //Print reference menu number, From: field, and time stamp
+                                    System.out.println((i+1) + ". From: " + msg.getFromUserName() + " at " + msg.getDateTime());
+                                }
+                                
+                                //Prompt user to input a menu number to read corresponding message
+                                System.out.println();
+                                System.out.print("Enter number to display message or any other key to return to main menu: ");
+                                //Get user input
+                                if (msgScan.hasNextInt())
+                                	msgNum = msgScan.nextInt();
+                                else 
+                                	break;
+                                
+                                System.out.println();
+                                
+                                //If valid menu input
+                                if(msgNum > 0 && msgNum <= msgCount)
+                                {
+                                    //Print the message requested and delete from linked list
+                                    msg = msgList.remove(msgNum - 1);
+                                    System.out.println("Message From " + msg.getFromUserName() + ": " + msg.getUserMsg());
+                                    System.out.println();
+                                    //Decrement unread messages
+                                    msgCount--;
+                                }
+                            }
+                            while(msgNum > 0 && msgNum <= msgCount);
+                        }
+                        
+                        
                         break;
 
                     case 8: //exit
